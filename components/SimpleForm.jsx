107b93c3be2a4e82e987336bde0eea741c46da57
Ange230700 <kouakouangeericstephane@gmail.com>
2023-11-07 11:50:50 +0100

    making a simple form

--- components/SimpleForm.jsx
+++ components/SimpleForm.jsx
@@ -2,10 +2,10 @@ function SimpleForm(props) {
     return (
         <form action="">
             <input name="uncontrolled" placeholder="I can be changed" />
-            <input name="controlled" placeholder="I can't be changed" />
+            <input name="controlled" value="I can't be changed" />
         </form>
     );
 }
-
+ù
 ReactDOM.render(<SimpleForm />, document.getElementById("root"));
 
